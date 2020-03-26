ea134d1055e3f991e98ad6d43dab9ede7ea20bb4
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-26 03:03:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,5 +6,5 @@ end
 
 def batch_badge_creator(names)
   array_names = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
-  array_names.each {|person| puts "Hello, my name is #{person}."}
+  array_names.each {|person| return "Hello, my name is #{person}."}
 end
