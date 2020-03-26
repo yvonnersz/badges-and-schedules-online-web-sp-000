8cd66fcb6345790a92a8c28e788fbd37b9b0db51
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-26 03:08:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,9 +6,5 @@ end
 
 def batch_badge_creator(names)
   array_names = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
-  counter = 1
-  array_names.each do |person|
-    counter +=1
-  end
-  puts "Hello, my name is #{person}."
+  array_names.each {|person| puts "Hello, my name is #{person}."}
 end
