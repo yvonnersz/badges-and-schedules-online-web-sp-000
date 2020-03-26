06bf1e1c73b394f2a571e6f312849b437a37e472
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-26 04:30:02 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badge_messages = []
+  names.each do |name|
+    badge_messages << badge_maker(name)
+  end
+  return badge_messages
+end
+
+def assign_rooms(names)
+  rooms = []
+  counter = 1
+  names.each do |name|
+    welcome_message = "Hello, #{name}! You'll be assigned to room #{counter}!"
+    counter +=1
+    rooms << welcome_message
+  end
+  return rooms
+end
+
+def printer(names)
+
+  batch_badge_creator(names).each do |i|
+    puts i
+  end
+
+  assign_rooms(names).each do |i|
+    puts i
+  end
+
+end
